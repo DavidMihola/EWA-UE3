5479712e4f5ed0439668a415f1536086ad8fc1b5
Florian Mihola <florian.mihola@gmail.com>
2011-05-16 10:32:20 +0200

    UserRegistrationBean erhält weitere Daten
    soll das Datum ein Date Objekt werden?
    sollen die weiter Userdaten bis zur Datenbank übergeben werden oder sollen sich die Benutzerobjekte ab jetzt auch aus der Datenbank herausholen lassen?

--- src/java/beans/UserRegistrationBean.java
+++ src/java/beans/UserRegistrationBean.java
@@ -23,8 +23,17 @@ public class UserRegistrationBean implements Serializable {
     
     private String username;
     private String password;
+    private int stackSize;
 
     private boolean additionalData;
+    private String firstname;
+    private String lastname;
+    private String dateOfBirth;
+
+    private enum Sex {
+        MALE, FEMALE
+    }
+    private Sex sex;
 
     public UserRegistrationBean() {
         additionalData = false;
@@ -42,16 +51,56 @@ public class UserRegistrationBean implements Serializable {
         this.username = username;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getUsername() {
         return username;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setStackSize(int stackSize) {
+        this.stackSize = stackSize;
+    }
+
+    public int getStackSize() {
+        return stackSize;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex.equals("m") ? Sex.MALE : Sex.FEMALE;
+    }
+
+    public String getSex() {
+        return (sex == Sex.MALE) ? "m" : "f";
     }
 
     public boolean getAdditionalData() {
